1b69f051696194faa58cf8d29204c626a9df4c50
Rosi <rosy.marttins@gmail.com>
2022-03-31 19:47:16 -0300

    again

--- src/main/java/com/generation/repository/ProdutoRepository.java
+++ src/main/java/com/generation/repository/ProdutoRepository.java
@@ -4,9 +4,12 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.generation.model.Produto;
 
+
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto,Long>{
 	
 	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
