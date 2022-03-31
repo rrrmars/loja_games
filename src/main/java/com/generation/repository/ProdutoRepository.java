ca4a05b4be6066ff9061a62093fffc68250c38ee
Rosi <rosy.marttins@gmail.com>
2022-03-31 19:43:03 -0300

    corrigido

--- src/main/java/com/generation/repository/ProdutoRepository.java
+++ src/main/java/com/generation/repository/ProdutoRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.model.Produto;
 
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+public interface ProdutoRepository extends JpaRepository<Produto,Long>{
 	
 	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
 
